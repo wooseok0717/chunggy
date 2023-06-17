9416df923f8bd67231456b8ed7535eede99c2ee4
Josh <wjang717@gmail.com>
2023-06-17 14:44:14 -0700

    added a feature not fully complete, to filter sets that are in database

--- client/src/components/Sub_Add/SetModal.jsx
+++ client/src/components/Sub_Add/SetModal.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 export default function SetModal({setClicked}) {
-  return (
 
+  const [filteredSet, setFilteredSet] = useState([]);
+
+  const handleChange = e => {
+    console.log(e.target.value)
+    axios.get(`api/set/${e.target.value}`)
+    .then(x => setFilteredSet(x.data))
+  }
+  return (
     <div className='modal'>
       <div className='modal-content'>
         <div className="modal-header">
           <h4 className='modal-title'>Search Set</h4>
         </div>
         <div className='modal-body'>
-          <input />
+          <input onChange={handleChange}/>
+          <div className='currentList'>
+          {filteredSet.map(setEntry => {
+            return (<button>{setEntry}</button>)
+          })}
+          </div>
         </div>
         <div className='modal-footer'>
           <button onClick={() => {setClicked(false)}}>close</button>
