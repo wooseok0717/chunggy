b60febc0f0c61b301978770e23b224bf90dd226c
Josh <wjang717@gmail.com>
2023-06-27 23:16:46 -0700

    implemented configuration of axios request and successfully sent to the server

--- server/controllers/sets.js
+++ server/controllers/sets.js
@@ -1,6 +1,21 @@
+const axios = require('axios');
+
+const baseURL = 'http://127.0.0.1:3000';
+
 module.exports ={
   createSet: (req, res) => {
-    console.log(req.body);
-    res.send('hello you just entered a new set info')
+    const config = {
+      method: 'post',
+      baseURL,
+      url: '/items/set',
+      data: req.body
+    }
+    axios(config)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      res.send('failed???');
+    })
   }
 }
